c53978f3c8a3e00ed97f0375be9e9be0f778b62f
lorellana672 <lorellana672@famaf.unc.edu.ar>
2018-10-21 04:40:58 -0300

    Problems whit the R :(

--- app/src/main/java/org/cloudhack/openlichen/MainActivity.java
+++ app/src/main/java/org/cloudhack/openlichen/MainActivity.java
@@ -1,12 +1,25 @@
 package org.cloudhack.openlichen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.new_report);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), MapActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
